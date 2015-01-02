111f60631fbfe05a8bec0a4737839e9cf0335c02
Tobias Strebitzer <tobias.strebitzer@gmail.com>
2015-01-02 11:13:44 +0800

    Created README.md; updated License information

--- Baker/src/main/java/com/bakerframework/baker/helper/ImageLoaderHelper.java
+++ Baker/src/main/java/com/bakerframework/baker/helper/ImageLoaderHelper.java
@@ -1,3 +1,29 @@
+/**
+ * Copyright (c) 2013-2014. Francisco Contreras, Holland Salazar.
+ * Copyright (c) 2015. Tobias Strebitzer, Francisco Contreras, Holland Salazar.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are
+ * permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list of
+ * conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice, this list of
+ * conditions and the following disclaimer in the documentation and/or other materials
+ * provided with the distribution.
+ * Neither the name of the Baker Framework nor the names of its contributors may be used to
+ * endorse or promote products derived from this software without specific prior written
+ * permission.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+ * SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ **/
 package com.bakerframework.baker.helper;
 
 import android.content.Context;
@@ -9,10 +35,6 @@ import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 
-/**
- * Created by Tobias Strebitzer <tobias.strebitzer@magloft.com> on 26/12/14.
- * http://www.magloft.com
- */
 public class ImageLoaderHelper {
 
 
@@ -29,13 +51,12 @@ public class ImageLoaderHelper {
                     .showImageOnFail(R.drawable.loading).cacheInMemory()
                     .cacheOnDisc().bitmapConfig(Bitmap.Config.RGB_565)
                     .build();
-            ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(
-                    context)
+            ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(context)
                     .defaultDisplayImageOptions(defaultOptions).build();
             ImageLoader.getInstance().init(config);
             return ImageLoader.getInstance();
         } catch (Exception ex) {
-            Log.e("ImageLoaderHelper", "Error when get image loader instance: " + ex);
+            Log.e("ImageLoaderHelper", "Error loading image: " + ex);
             return null;
         }
     }
---
+++ README.md
@@ -0,0 +1,60 @@
+# Baker Android App  [![Google Play](http://developer.android.com/images/brand/en_generic_rgb_wo_45.png)](https://play.google.com/store/apps/details?id=com.magloft.demo) [![Build Status](https://travis-ci.org/bakerframework/baker-android-refactor.png)](https://travis-ci.org/bakerframework/baker-android-refactor)
+
+The HTML5 ebook framework to publish interactive books & magazines on any Android device using simply open web standards http://bakerframework.com
+
+[![Download from Google Play](http://magloft-static.s3.amazonaws.com/baker-screen.png)](https://play.google.com/store/apps/details?id=com.magloft.demo)
+
+
+Please see the [issues](https://github.com/bakerframework/baker-android-refactor/issues) section to
+report any bugs or feature requests and to see the list of known issues.
+
+## Building
+
+### With Gradle
+
+The easiest way to build is to install [Android Studio](https://developer.android.com/sdk/index.html) v1.+
+with [Gradle](https://www.gradle.org/) v2.2.1.
+Once installed, then you can import the project into Android Studio:
+
+1. Open `File`
+2. Import Project
+3. Select `build.gradle` under the project directory
+4. Click `OK`
+
+Then, Gradle will do everything for you.
+
+## Acknowledgements
+
+This project is based on the original [Baker Android](https://github.com/bakerframework/baker-android) project by @fcontreras, @hsalazarl and @nin9creative.
+
+This project follows the features and roadmap of the official [Baker iOS Project](https://github.com/bakerframework/baker), currently maintained by @pieterclaerhout, @nin9creative and the baker community.
+
+It also uses other great open source libraries such as:
+
+* [jsoup](https://github.com/jhy/jsoup)
+* [Android Checkout Library](https://github.com/serso/android-checkout)
+* [Google GSON](https://code.google.com/p/google-gson)
+* [Universal Image Loader](https://github.com/nostra13/Android-Universal-Image-Loader)
+* [ViewPagerIndicator](https://github.com/JakeWharton/Android-ViewPagerIndicator)
+
+## Contributing
+
+Please fork this repository and contribute back using
+[pull requests](https://github.com/bakerframework/baker-android-refactor/pulls).
+
+Any kind of contribution, features, bug fixes, language translations, unit/integration tests, refactors are welcome and appreciated but will be thoroughly reviewed and discussed.
+
+Please make sure that:
+
+* Your pull request is based on the latest possible version of the Baker Android `master` branch
+* You have provided a meaningful title for your pull request
+* You have briefly explained in the pull request what it does and why it should be merged
+* You have referenced any relevant issue
+
+## License
+
+This software is free to use under the BSD license.
+See the [LICENSE file][] for license text and copyright information.
+
+
+[LICENSE file]: https://github.com/bakerframework/baker-android-refactor/blob/master/LICENSE.md
